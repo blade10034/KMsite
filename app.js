32758de0f6ab3871bdacaf06b09b3addd8a844d3
Sean <sfortner001@gmail.com>
2016-12-13 23:42:33 -0500

    Convert to angular2 setup

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+//var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -9,55 +9,44 @@ LocalStrategy = require('passport-local').Strategy;
 var session = require('express-session');
 var http = require('http');
 var multer = require('multer');
-var isAuth = require('./Util/isAuth.js');
-
-var routes = require('./routes/index');
-var events = require('./routes/events');
-var sermons = require('./routes/sermons');
-var gallery = require('./routes/gallery');
-var announcement = require('./routes/announcement');
-var fileRouter = require('./routes/files');
+var isAuth = require('./api/Util/isAuth.js');
+var events = require('./api/routes/events');
+var sermons = require('./api/routes/sermons');
+var gallery = require('./api/routes/gallery');
+var announcement = require('./api/routes/announcement');
+var fileRouter = require('./api/routes/files');
 var range = require('express-range');
-
 var app = express();
 var mongoose = require('mongoose');
 var Grid = require('gridfs-stream');
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+var Account = require('./api/schema/account');
+
 var conn = mongoose.createConnection('localhost:27017/KmDb');
-var db = conn.db;
 
 conn.once('open', function(){
   var gfs = Grid(conn.db, mongoose.mongo);
   app.set('gridfs', gfs);
 });
 
-mongoose.connect('localhost:27017/KmDb');
+//mongoose.connect('localhost:27017/KmDb');
 
+// Setup express routes
+app.use(sermons);
+app.use(events);
+app.use(announcement);
+app.use(gallery);
+app.use(fileRouter);
 
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(function(req,res,next){
-    req.db = db;
-    next();
-});
-
-app.use(multer({
+//Setup multer file upload location and onError
+multer({
   dest: "./uploads/",
   onError: function (error, next) {
 	  console.log(error)
 	  next(error)
 	}
-}));
+});
 
+// Setup language cookie
 app.use(function(req, res, next) {
 	if(req.cookies == undefined){
 		res.cookie('language', 'English');
@@ -67,19 +56,12 @@ app.use(function(req, res, next) {
 	next();
 });
 
+//Handle Content-Range header
 app.use(range({
   accept: 'bytes'
 }));
 
-app.use('/', routes);
-app.use('/sermons', sermons);
-app.use('/events', events);
-app.use('/announcement', announcement);
-app.use('/gallery', gallery);
-app.use('/media', fileRouter);
 
-
-var Account = require('./schema/account');
 // Authentication Layer
 passport.use(new LocalStrategy(Account.authenticate()));
 passport.serializeUser(Account.serializeUser());
@@ -97,17 +79,29 @@ if (app.get('env') === 'development') {
             error: err
         });
     });
+}else{
+  // production error handler
+  // no stacktraces leaked to user
+  app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error/error', {
+          message: err.message,
+          error: {}
+      });
+  });
 }
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error/error', {
-        message: err.message,
-        error: {}
-    });
+app.use('/scripts', express.static(path.join(__dirname, '/node_modules/')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', function (req, res, next) {
+    res.sendFile('./public/index.html');
 });
-
 
 module.exports = app;
